a773c15bb06d8b6a6030282274c41ec51097c676
Ana Chua <anaprimawaty@gmail.com>
2017-08-17 17:51:23 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+	res.send("hello world");
+});
+
+var server = app.listen(8081, "127.0.0.1", function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log("pixelect listening at http://%s:%s", host, port)
+});
