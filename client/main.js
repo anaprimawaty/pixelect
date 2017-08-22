a8efba72f8e16e74d910c6ba763bcb2023f18730
Goh Wei Wen <goweiwen@gmail.com>
2017-08-22 22:02:12 +0800

    Add link copying

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 import VueProgressiveImage from 'vue-progressive-image'
+import Toasted from 'vue-toasted'
 import App from '@/App'
 import router from '@/router'
 import store from '@/store'
@@ -12,6 +13,7 @@ import 'buefy/lib/buefy.css'
 
 Vue.use(Buefy)
 Vue.use(VueProgressiveImage)
+Vue.use(Toasted)
 
 // eslint-disable-next-line no-new
 new Vue({
--- client/views/Group.vue
+++ client/views/Group.vue
@@ -9,12 +9,13 @@
       </span>
     </div>
     <div class="group-link">
-      <a :href="link">{{ link }}</a>
-      <span class="icon">
+      <a :href="link" @click="clickLink">{{ link }}</a>
+      <span class="icon" @click="clickLink">
         <svg style="width:16px;height:16px" viewBox="0 0 24 24">
           <path fill="#a0a0a0" d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
         </svg> 
       </span>
+      <input ref="link" :value="link" />
     </div>
     <photo-list :photos="photos" />
   </div>
@@ -67,6 +68,22 @@ export default {
       }
       store.dispatch(UPDATE_GROUP_NAME, name)
     },
+    clickLink(e) {
+      this.$refs.link.select()
+      let successful = false
+      try {
+        successful = document.execCommand('copy')
+      } catch (err) {}
+      if (!successful) {
+        prompt('Copy this link', this.$refs.link.value)
+      } else {
+        this.$toasted.show('Copied link!', {
+          theme: 'outline',
+          position: 'top-center',
+          duration: 5000,
+        })
+      }
+    },
   },
 }
 </script>
@@ -81,10 +98,17 @@ export default {
 .group-link a {
   color: #a0a0a0;
   text-decoration: none;
-  Border-bottom: 1px dashed #a0a0a0;
+  border-bottom: 1px dashed #a0a0a0;
+}
+
+.group-link input {
+  width: 1em;
+  height: 1em;
+  padding: 0;
+opacity: 0;
 }
 
 .group-link {
-  margin-bottom: 1em;
+  margin-bottom: 1.5em;
 }
 </style>
