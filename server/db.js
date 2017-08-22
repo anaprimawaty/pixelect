8f73451a229a1747abdb14602090db5ad079ed4b
Ana Chua <anaprimawaty@gmail.com>
2017-08-22 14:20:22 +0800

    Schema for User, Group and User-Group

--- server/db.js
+++ server/db.js
@@ -1,13 +1,73 @@
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
+// Establish connection to database
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.PIXELECT_STAGING_DATABASE, process.env.PIXELECT_STAGING_USER, process.env.PIXELECT_STAGING_PASSWORD, {
   host: process.env.PIXELECT_STAGING_HOST,
   port: process.env.PIXELECT_STAGING_PORT,
-  user: process.env.PIXELECT_STAGING_USER,
-  password: process.env.PIXELECT_STAGING_PASSWORD,
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 2,
+    idle: 10000
+  }
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Staging Database Connected!");
+// Test connection to database
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Successfully connected to database.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to database:', err);
+  });
+
+// User table
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  facebookId: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
+
+// Group table
+const Group = sequelize.define('group', {
+});
+Group.belongsTo(User, {
+  foreignKey: {
+    name: 'owner',
+    allowNull: false
+  },
+  onDelete: 'CASCADE'
+});
+
+// User-Group table
+Group.belongsToMany(User, {through: 'UserGroup'});
+User.belongsToMany(Group, {through: 'UserGroup'});
+
+// API stuff for future
+// To create User
+// User.create({
+//   firstName: 'Ana',
+//   lastName: 'Chua',
+//   facebookId: '123'
+// });
+
+// To create Group
+// Group.create({
+//   owner: 1
+// });
+
+// To add User to Group
+// User.findById(3).then(user => {
+//   Group.findById(1).then(group => {
+//     user.setGroups([group]);
+//   });
+// });
