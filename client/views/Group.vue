23bb50b1b8e0a1e77c94ba6f2871f16c2d42a0b2
Goh Wei Wen <goweiwen@gmail.com>
2017-08-25 16:54:51 +0800

    Add share link button

--- client/views/Group.vue
+++ client/views/Group.vue
@@ -9,10 +9,15 @@
       </span>
     </div>
     <div class="group-link">
-      <a :href="link" @click="clickLink">{{ link }}</a>
-      <span class="icon" @click="clickLink">
-        <svg style="width:16px;height:16px" viewBox="0 0 24 24">
+      <a :href="link" @click="copyLink">{{ link }}</a>
+      <span class="icon action" @click="copyLink">
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
           <path fill="#a0a0a0" d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
+        </svg>
+      </span>
+      <span class="icon action" @click="shareLink">
+        <svg style="width:28px;height:28px;margin-top:-2px" viewBox="0 0 512 512">
+          <path fill="#a0a0a0" d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z" />
         </svg>
       </span>
       <input ref="link" :value="link" />
@@ -76,7 +81,14 @@ export default {
       }
       store.dispatch(UPDATE_GROUP_NAME, { groupId: this.groupId, name })
     },
-    clickLink(e) {
+    shareLink(e) {
+      FB.ui({
+        method: 'send',
+        link: this.$refs.link.value,
+      })
+      console.log(this.$refs.link.value)
+    },
+    copyLink(e) {
       this.$refs.link.select()
       let successful = false
       try {
@@ -117,6 +129,10 @@ export default {
   height: 1em;
   padding: 0;
   opacity: 0;
+}
+
+.action {
+  cursor: pointer;
 }
 
 .group-link {
