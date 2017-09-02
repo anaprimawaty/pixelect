54ec6ca75b4d8e7f1c6cb559b613b2dd160e61c4
Ana Chua <anaprimawaty@gmail.com>
2017-09-02 16:14:35 +0800

    Clean users.js

--- server/helper.js
+++ server/helper.js
@@ -1,29 +1,44 @@
 module.exports = {
+  log: function(source, msg) {
+    var date = new Date().toUTCString()
+    console.log("\n[" + date + "]" + source + " " + msg + "\n")
+  },
+
+  success: function() {
+    return JSON.stringify({'Success':''})
+  },
+
+  error: function(e) {
+    return JSON.stringify({'Error':e})
+  },
+
   getHash: function(id) {
     var data = new Date() + id;
     var hash = require('crypto').createHash('md5').update(data).digest('hex');
     return hash
   },
 
-  getUserId: function(models, facebookId) {
-    models.User.findOne({
-      where: { facebookId: facebookId }
+  getUser: function(models, facebookId) {
+    return new Promise(function(resolve, reject) {
+      models.User.findOne({
+        where: { facebookId: facebookId }
+      })
+      .then(user => {
+        if (user) {
+          resolve(user)
+        } else {
+          reject('Error: No User with facebookId:' + facebookId)
+        }
+      })
     })
-    .then(user => {
-      return user.get('id');
-    })
-    .catch(e => {
-      console.log("Error finding user with facebookId=" + facebookId + ": " + e);
-      throw "Error finding user with facebookId=" + facebookId;
-    });
   },
 
-  getGroupId: function(models, groupHash) {
+  getGroup: function(models, groupHash) {
     models.Group.findOne({
       where: { hash: groupHash }
     })
     .then(group => {
-      return group.get('id');
+      return group;
     })
     .catch(e => {
       console.log("Error finding group with groupHash=" + groupHash + ": " + e);
--- server/users.js
+++ server/users.js
@@ -1,64 +1,60 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var helper = require('./helper')
 
-
-/* GET user groups with user ID
- * params -> groupId: id of the group
- * response -> {groups}
+/* GET get groups of user
+ * body -> session.facebookId
+ * response -> groups/error
  */
 router.get('/groups', function(req, res) {
-	var models = req.app.get('models')
-	var session = req.app.get('session')
-	models.User.findOne({
-	    where: {
-	      facebookId: session.facebookId
-	    }
-	})
-	.then(user => {
-	 	user.getGroups()
-	 	.then(groups => {
-	 		res.send(groups);
-	 	})
-	 	.catch(e => {
-	 		console.log(e);
-	 		res.send("Error finding groups of user");
-		});
-	 })
-	 .catch(e => {
-	 	console.log(e);
-	 	res.send("Error finding user");
-	 });
-});
-
-/* POST add user to database, set userId to session
- * body -> {name, facebookId}
- * response -> success/error
- */
-router.post('/', function(req, res){
-	var models = req.app.get('models')
-	var session = req.app.get('session')
-	models.User.findOne({
-	    where: {
-	      facebookId: session.facebookId
-	    }
-	}).then(function(user){
-		session.facebookId = req.body.facebookId
-		res.send('logged in')
-	})
-	.catch(e => {
-	 	console.log('new user')
-	 	models.User.create({
-			firstName: req.body.name,
-			lastName: "null",
-			facebookId: req.body.facebookId
-		}).then(function(){
-			res.send('signed up')
-			session.facebookId = req.body.facebookId
-		}, function(error){
-			console.log(error)
-			res.send('error setting user')
-		})
-	 })
+  var source = '[GET /users/groups]'
+  var models = req.app.get('models')
+  var session = req.app.get('session')
+  helper.getUser(models, session.facebookId)
+  .then(user => {
+    user.getGroups()
+    .then(groups => {
+      helper.log(source, 'Success: userId:' + user.id)
+      res.send(groups)
+    })
+  })
+  .catch(e => {
+    helper.log(source, e)
+    res.status(500).send(helper.error(e))
+  })
 })
 
-module.exports = router;
+/* POST set session.facebookId. create user if user does not exist
+ * body -> facebookId, name
+ * response -> success/error
+ */
+router.post('/', function(req, res) {
+  var source = '[POST /users/]'
+  var models = req.app.get('models')
+  var session = req.app.get('session')
+  var facebookId = req.body.facebookId
+  helper.getUser(models, facebookId)
+  .then(user => {
+    session.facebookId = facebookId
+    helper.log(source, 'Success: userId:' + user.id + ' logged in')
+    res.send(helper.success())
+  })
+  .catch(e => {
+    models.User.create({
+      firstName: req.body.name,
+      lastName: 'null',
+      facebookId: facebookId,
+    })
+    .then(user => {
+      session.facebookId = facebookId
+      helper.log(source, 'Success: userId:' + user.id + ' signed up')
+      res.send(helper.success())
+    })
+    .catch(e => {
+      helper.log(source, e)
+      res.status(500).send(helper.error(e))
+    })
+  })
+})
+
+module.exports = router
