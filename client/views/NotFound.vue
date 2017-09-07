7e24e7bb495ce74bf00b867edcd47e553915742b
Ana Chua <anaprimawaty@gmail.com>
2017-09-08 01:01:17 +0800

    Attempt 404 page

--- client/views/NotFound.vue
+++ client/views/NotFound.vue
@@ -1,7 +1,9 @@
 <template>
-	<div>
+	<div class="error">
 		<span>
-			Page not found
+			<img src="/assets/404.png"/>
+			<span class="text">OH, SNAP! 404</span>
+			<router-link :to="`/`"><span class="redirect">Take me to safety!</span></router-link>
 		</span>
 	</div>
 </template>
@@ -13,4 +15,19 @@ export default {
 </script>
 
 <style scoped>
+.error {
+	padding-top: 50px;
+	text-align: center;
+}
+.text {
+	font-size: 50px;
+	display:block;
+	padding-bottom: 30px;
+}
+.redirect {
+	font-size: 20px;
+	border-radius: 5px;
+	border: 1px solid;
+	padding: 6px;
+}
 </style>
