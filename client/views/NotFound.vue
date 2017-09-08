a52b90e34ac45a081efd48347db339afe0439b7d
Si Kai <sikai.ngsk@gmail.com>
2017-09-08 14:43:31 +0800

    added footer to error page

--- client/views/NotFound.vue
+++ client/views/NotFound.vue
@@ -1,29 +1,43 @@
 <template>
-	<div class="error">
-		<span>
-			<img class="camera" src="/assets/404.png"/>
-			<img class="snap" src="/assets/404-snap.png"/>
-			<h1 class="title">OH, SNAP!</h1>
-			<h2 class="subtitle">This page doesn't exist</h2>
-			<router-link class="button is-primary is-large" :to="`/`">Take me to safety!</router-link>
-		</span>
-	</div>
+  <div>
+    <div class="container">
+      <section class="section">
+        <div class="error has-text-centered">
+    			<img class="camera" src="/assets/404.png"/>
+    			<img class="snap" src="/assets/404-snap.png"/>
+    			<h1 class="title">OH, SNAP!</h1>
+    			<h2 class="subtitle">This page doesn't exist</h2>
+    			<router-link class="button is-primary" :to="`/`">Take me to safety!</router-link>
+        </div>
+      </section>
+    </div>
+    <custom-footer/>
+  </div>
 </template>
 
 <script>
+import CustomFooter from '@/components/CustomFooter'
+
 export default {
   name: 'NotFound',
+  components: {
+    CustomFooter,
+  },
 }
 </script>
 
 <style scoped>
+
 .error {
-	position: relative;
-	padding-top: 70px;
-	text-align: center;
+  margin-top: 2.2rem;
+}
+
+.title {
+  margin-top:0.5rem;
 }
 
 .camera {
+  height: 15rem;
 	margin-bottom: 1em;
 }
 
@@ -35,21 +49,12 @@ export default {
 
 .snap {
 	position: absolute;
-	top: 20px;
+	top: 45px;
 	left: 0;
 	right: 0;
 	margin: auto;
 	pointer-events: none;
 	animation: blink 2s infinite step-start;
-}
-
-.title {
-	font-size: 3em;
-	margin-bottom: 1em;
-}
-
-.subtitle {
-	font-size: 2em;
-	margin-bottom: 1em;
+  height: 7rem;
 }
 </style>
